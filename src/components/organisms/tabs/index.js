59d557711e480d03411c25e027ac6e07100adb83
schettn <nicoschett@icloud.com>
2020-06-06 18:32:31 +0200

    Add index file to tabs
    A file has been added to import all 
    components to export them for easy access
    from parent components.

---
+++ src/components/organisms/tabs/index.js
@@ -0,0 +1,17 @@
+//#region > Imports
+//> Components
+// Import all components to export them for easy access from parent components
+import ProjectTab from "./ProjectTab";
+import OverviewTab from "./OverviewTab";
+import TalksTab from "./TalksTab";
+//#endregion
+
+//#region > Exports
+// Export the components for easy access from parent components
+export { ProjectTab, OverviewTab, TalksTab };
+//#endregion
+
+/**
+ * SPDX-License-Identifier: (EUPL-1.2)
+ * Copyright © Simon Prast
+ */
