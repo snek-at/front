512dcdfacb75b6568d250d834774c1856ac8f449
schettn <nicoschett@icloud.com>
2020-06-06 18:36:16 +0200

    Add SoftwareEngineer Component
    The SoftwareEngineer Component has
    been added.

---
+++ src/components/organisms/profiles/SoftwareEngineer/index.jsx
@@ -0,0 +1,44 @@
+//#region > Imports
+//> React
+// Contains all the functionality necessary to define React components
+import React from "react";
+
+//> MDB
+// "Material Design for Bootstrap" is a great UI design framework
+import { MDBContainer, MDBRow, MDBCol } from "mdbreact";
+
+//> Components
+import { SoftwareTabs, ProfileInfo } from "../../index";
+//#endregion
+
+//#region > Components
+class SoftwareEngineer extends React.Component {
+  state = {};
+
+  render() {
+    const { globalState, globalFunctions } = this.props;
+
+    return (
+      <MDBContainer className="py-5">
+        <MDBRow>
+          <MDBCol lg="3">
+            <ProfileInfo globalState={globalState} />
+          </MDBCol>
+          <MDBCol lg="9">
+            <SoftwareTabs globalState={globalState} />
+          </MDBCol>
+        </MDBRow>
+      </MDBContainer>
+    );
+  }
+}
+//#endregion
+
+//#region > Exports
+export default SoftwareEngineer;
+//#endregion
+
+/**
+ * SPDX-License-Identifier: (EUPL-1.2)
+ * Copyright © Simon Prast
+ */
---
+++ src/components/organisms/profiles/index.js
@@ -0,0 +1,15 @@
+//#region > Imports
+//> Components
+// Import all components to export them for easy access from parent components
+import SoftwareEngineer from "./SoftwareEngineer";
+//#endregion
+
+//#region > Exports
+// Export the components for easy access from parent components
+export { SoftwareEngineer };
+//#endregion
+
+/**
+ * SPDX-License-Identifier: (EUPL-1.2)
+ * Copyright © Simon Prast
+ */
