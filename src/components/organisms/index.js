47d1701058aac08e75f245c0fbdc5dc4cba409c0
Pinterid <david.pinterics@gmail.com>
2020-06-06 12:00:04 +0200

    Add folder structure organisms
    The folder structure for organisms was added.

---
+++ src/components/organisms/index.js
@@ -0,0 +1,14 @@
+//#region > Imports
+//> Organisms
+// Import all components to export them for easy access from parent components
+//import Organism from "./Organism";
+//#endregion
+
+//#region > Exports
+//export { Organism };
+//#endregion
+
+/**
+ * SPDX-License-Identifier: (EUPL-1.2)
+ * Copyright © Simon Prast
+ */
