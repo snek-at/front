00268f9e9363b2c22a8b839781fffb4bc1f7fa81
schettn <nicoschett@icloud.com>
2020-06-06 21:26:46 +0200

    Add modals index
    Implemented for export them for
    easy access from parent components.

---
+++ src/components/molecules/modals/index.js
@@ -0,0 +1,15 @@
+//#region > Imports
+//> Components
+// Import all components to export them for easy access from parent components
+import UploadModal from "./UploadModal";
+//#endregion
+
+//#region > Exports
+// Export the components for easy access from parent components
+export { UploadModal };
+//#endregion
+
+/**
+ * SPDX-License-Identifier: (EUPL-1.2)
+ * Copyright © Simon Prast
+ */
