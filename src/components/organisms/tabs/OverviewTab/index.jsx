b916bcd56c4fe909561989ef1bf48afb61dff298
schettn <nicoschett@icloud.com>
2020-07-07 10:23:42 +0200

    Integrate Movable in Overview Tab
    A editor mode has been added which allows movement for all relevant
    components.

--- src/components/organisms/tabs/OverviewTab/index.jsx
+++ src/components/organisms/tabs/OverviewTab/index.jsx
@@ -19,6 +19,8 @@ import {
   LatestActivity,
   ErrorBoundary,
 } from "../../../atoms";
+import { MovableBoundary } from "../../../molecules";
+
 //#endregion
 
 //#region > Constant Variables
@@ -67,126 +69,173 @@ class OverviewTab extends React.Component {
   };
 
   render() {
-    const { platformData } = this.props;
+    const { platformData, sameOrigin } = this.props;
 
     return (
       <>
         {/* <ErrorBoundary hidden="true"> */}
         <ErrorBoundary>
-          {platformData && (
-            <MDBRow className="text-center text-md-left mb-4">
-              {platformData.statistic.languages.map((language, i) => {
-                if (i < 6) {
-                  return (
-                    <MDBCol md="4" key={i}>
-                      <span className="mb-2 text-muted">
-                        <MDBIcon
-                          icon="square"
-                          className="pr-2"
-                          style={{ color: language.color }}
-                        />
-                        {language.name} <small>{language.share}%</small>
-                      </span>
-                    </MDBCol>
-                  );
-                }
-              })}
-            </MDBRow>
+          {sameOrigin && (
+            <div className="text-right">
+              <MDBBtn
+                color={this.state.edit ? "success" : "green"}
+                size="md"
+                onClick={() => this.setState({ edit: !this.state.edit })}
+              >
+                {this.state.edit ? (
+                  <span>
+                    <MDBIcon icon="check-circle" />
+                    Save items
+                  </span>
+                ) : (
+                  <span>Move items</span>
+                )}
+              </MDBBtn>
+            </div>
           )}
-          {false && (
-            <>
-              <MDBRow className="m-0 p-0">
-                <MDBCol md="6 text-left">
-                  <p className="lead">Pinned</p>
-                </MDBCol>
-                <MDBCol md="6 text-right">
-                  <span className="clickable text-muted">Customize</span>
-                </MDBCol>
-              </MDBRow>
-              <MDBRow className="pinned">
-                {pinned.map((item, i) => {
-                  return <Pinned {...item} key={i} />;
-                })}
-              </MDBRow>
-            </>
-          )}
-          {platformData &&
-            (platformData.user.settings.show3DDiagram ||
-              platformData.user.settings.show2DDiagram) && (
-              <div className="text-right year-select">
-                {platformData.statistic.years.map((year, i) => {
-                  return (
-                    <MDBBtn
-                      color="white"
-                      key={i}
-                      size="md"
-                      className={
-                        year.year === this.state.selectedYear
-                          ? "selected"
-                          : undefined
+          <MovableBoundary
+            uid="overview"
+            edit={this.state.edit}
+            items={[
+              <>
+                {platformData && (
+                  <MDBRow className="text-center text-md-left mb-4">
+                    {platformData.statistic.languages.map((language, i) => {
+                      if (i < 6) {
+                        return (
+                          <MDBCol md="4" key={i}>
+                            <span className="mb-2 text-muted">
+                              <MDBIcon
+                                icon="square"
+                                className="pr-2"
+                                style={{ color: language.color }}
+                              />
+                              {language.name} <small>{language.share}%</small>
+                            </span>
+                          </MDBCol>
+                        );
                       }
-                      onClick={() => this.setState({ selectedYear: year.year })}
-                    >
-                      {year.year}
-                    </MDBBtn>
-                  );
-                })}
-                <MDBBtn
-                  color="white"
-                  size="md"
-                  className={
-                    this.state.selectedYear === undefined
-                      ? "selected"
-                      : undefined
-                  }
-                  onClick={() => this.setState({ selectedYear: undefined })}
-                >
-                  Current
-                </MDBBtn>
-              </div>
-            )}
-          {platformData && platformData.user.settings.show3DDiagram && (
-            <Calendar3D
-              platformData={platformData}
-              year={this.state.selectedYear}
-            />
-          )}
-          {platformData && platformData.user.settings.show2DDiagram && (
-            <Calendar2D
-              platformData={platformData}
-              year={this.state.selectedYear}
-              selectDay={this.selectDay}
-            />
-          )}
-          <MDBRow className="mt-4">
-            <MDBCol md="7">
-              <p className="lead">Contribution Types</p>
-              {platformData && !platformData.user.settings.showContribDiagram && (
-                <div className="mt-5">
-                  <ContribRadar
-                    statistic={platformData.statistic}
+                    })}
+                  </MDBRow>
+                )}
+              </>,
+              <>
+                {false && (
+                  <>
+                    <MDBRow className="m-0 p-0">
+                      <MDBCol md="6 text-left">
+                        <p className="lead">Pinned</p>
+                      </MDBCol>
+                      <MDBCol md="6 text-right">
+                        <span className="clickable text-muted">Customize</span>
+                      </MDBCol>
+                    </MDBRow>
+                    <MDBRow className="pinned">
+                      {pinned.map((item, i) => {
+                        return <Pinned {...item} key={i} />;
+                      })}
+                    </MDBRow>
+                  </>
+                )}
+              </>,
+              <>
+                {platformData &&
+                  (platformData.user.settings.show3DDiagram ||
+                    platformData.user.settings.show2DDiagram) && (
+                    <div className="text-right year-select">
+                      {platformData.statistic.years.map((year, i) => {
+                        return (
+                          <MDBBtn
+                            color="white"
+                            key={i}
+                            size="md"
+                            className={
+                              year.year === this.state.selectedYear
+                                ? "selected"
+                                : undefined
+                            }
+                            onClick={() =>
+                              this.setState({ selectedYear: year.year })
+                            }
+                          >
+                            {year.year}
+                          </MDBBtn>
+                        );
+                      })}
+                      <MDBBtn
+                        color="white"
+                        size="md"
+                        className={
+                          this.state.selectedYear === undefined
+                            ? "selected"
+                            : undefined
+                        }
+                        onClick={() =>
+                          this.setState({ selectedYear: undefined })
+                        }
+                      >
+                        Current
+                      </MDBBtn>
+                    </div>
+                  )}
+              </>,
+              <>
+                {platformData && platformData.user.settings.show3DDiagram && (
+                  <Calendar3D
+                    platformData={platformData}
                     year={this.state.selectedYear}
                   />
-                </div>
-              )}
-            </MDBCol>
-            <MDBCol md="5">
-              <p className="lead">
-                Activity
-                <MDBIcon
-                  icon="angle-double-up"
-                  className="green-text ml-2"
-                  size="md"
+                )}
+              </>,
+              <>
+                {platformData && platformData.user.settings.show2DDiagram && (
+                  <Calendar2D
+                    platformData={platformData}
+                    year={this.state.selectedYear}
+                    selectDay={this.selectDay}
+                  />
+                )}
+              </>,
+              <div className="interchange-charts">
+                <MovableBoundary
+                  uid="contribtype"
+                  edit={this.state.edit}
+                  items={[
+                    <>
+                      <p className="lead">Contribution Types</p>
+                      {platformData &&
+                        !platformData.user.settings.showContribDiagram && (
+                          <div className="mt-5">
+                            <ContribRadar
+                              statistic={platformData.statistic}
+                              year={this.state.selectedYear}
+                            />
+                          </div>
+                        )}
+                    </>,
+                    <>
+                      <p className="lead">
+                        Activity
+                        <MDBIcon
+                          icon="angle-double-up"
+                          className="green-text ml-2"
+                          size="md"
+                        />
+                      </p>
+                      <p className="text-muted mb-0">Weekly overview</p>
+                      <LatestActivity
+                        statistic={platformData.statistic}
+                        year={this.state.selectedYear}
+                        activity={this.state.activity}
+                      />
+                    </>,
+                  ]}
+                  movementAxis="x"
                 />
-              </p>
-              <p className="text-muted mb-0">Weekly overview</p>
-              <LatestActivity
-                statistic={platformData.statistic}
-                year={this.state.selectedYear}
-                activity={this.state.activity}
-              />
-            </MDBCol>
-          </MDBRow>
+              </div>,
+            ]}
+            movementAxis="y"
+          />
         </ErrorBoundary>
       </>
     );
